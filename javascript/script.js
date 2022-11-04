f92d4c8258f45752da1615e2a0dbbf168e8a5591
Ali Muratkali <ali@muratka.li>
2022-11-04 19:47:07 +0600

    feat: close-button works on popup on click

--- javascript/script.js
+++ javascript/script.js
@@ -1,13 +1,19 @@
 const container = document.querySelector('.content');
-const editButton = container.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
+const editButton = document.querySelector('.profile__edit-button');
+const closeButton = document.querySelector('.popup__close-button');
 
 
 function openPopup() {
     popup.classList.add('popup_opened');
   }
+
+function closePopup() {
+    popup.classList.remove('popup_opened');
+}
   
-editButton.addEventListener('click', openPopup);
+editButton.addEventListener ('click', openPopup);
+closeButton.addEventListener ('click', closePopup);
 
 //   editButton.addEventListener('click', function () {
 //     openPopup(popup);
