7b3f83c8f02af5f0d11a375fa58c3d3040a91040
Ali Muratkali <ali@muratka.li>
2022-11-06 14:37:11 +0600

    refactor: deleted unnecessary gaps, moved default images array to constant.js, used openPopup and closePopup functions to open and close Popups, renamed addNewItem and handleFormSubmit functions, added Img.alt feature

---
+++ javascript/constants.js
@@ -0,0 +1,27 @@
+//  дефолтные картинки 
+const initialCards = [
+    {
+      name: 'Finally Rich (2012)',
+      link: 'https://www.udiscovermusic.com/wp-content/uploads/2022/07/aChiefKe_Finally_CoverAr_3000DPI300RGB1000132005.jpg'
+    },
+    {
+      name: 'Back from the Dead 2',
+      link: 'https://m.media-amazon.com/images/I/71FjT-7fSOS._AC_SL1200_.jpg'
+    },
+    {
+      name: '4NEM',
+      link: 'https://media.pitchfork.com/photos/61b8f491517e91d45ad2f88f/3:2/w_1500,h_1000,c_limit/Chief-Keef-4Nem.jpeg'
+    },
+    {
+      name: 'Almighty So',
+      link: 'https://pbs.twimg.com/media/EMXyLcbUEAIjRqr.jpg'
+    },
+    {
+      name: 'GloFiles, Pt.3',
+      link: 'https://images.genius.com/33f6a00f3af54eb069fa52876fbb35b3.1000x1000x1.png'
+    },
+    {
+      name: 'GloToven',
+      link: 'https://cdn.albumoftheyear.org/album/144222-glotoven.jpg'
+    }
+  ]; 
--- javascript/script.js
+++ javascript/script.js
@@ -1,44 +1,17 @@
 
-const initialCards = [
-    {
-      name: 'Finally Rich (2012)',
-      link: 'https://www.udiscovermusic.com/wp-content/uploads/2022/07/aChiefKe_Finally_CoverAr_3000DPI300RGB1000132005.jpg'
-    },
-    {
-      name: 'Back from the Dead 2',
-      link: 'https://m.media-amazon.com/images/I/71FjT-7fSOS._AC_SL1200_.jpg'
-    },
-    {
-      name: '4NEM',
-      link: 'https://media.pitchfork.com/photos/61b8f491517e91d45ad2f88f/3:2/w_1500,h_1000,c_limit/Chief-Keef-4Nem.jpeg'
-    },
-    {
-      name: 'Almighty So',
-      link: 'https://pbs.twimg.com/media/EMXyLcbUEAIjRqr.jpg'
-    },
-    {
-      name: 'GloFiles, Pt.3',
-      link: 'https://images.genius.com/33f6a00f3af54eb069fa52876fbb35b3.1000x1000x1.png'
-    },
-    {
-      name: 'GloToven',
-      link: 'https://cdn.albumoftheyear.org/album/144222-glotoven.jpg'
-    }
-  ]; 
-  
 
 // переменные
 
 // формы
 const popupEditProfileForm = document.querySelector('.form__edit-profile-bio');
-const addItemForm = document.querySelector('.form__add-item');
+const formAddCard = document.querySelector('.form__add-item');
 
 //попапы
 const popupEditProfile = document.querySelector('.popup_type_edit-profile');
 const popupAddItem = document.querySelector('.popup_type_add-item');
 const popupFullscreenItem = document.querySelector('.popup_type_fullscreen');
 
-//
+//инпуты для формы 
 const profileName = document.querySelector('.profile__name');
 const profileSubtitle = document.querySelector('.profile__subheading');
 const profileNameInput = document.querySelector('.form_input_name');
@@ -62,15 +35,13 @@ const popupItemTitle = document.querySelector('.popup__fullscreen-title');
 const elementsTemplate = document.querySelector('.template').content.querySelector(".elements__item");
 const elementsList = document.querySelector('.elements__list');
 
-
-
-// открытие попапа ??
+// открытие любого попапа 
 function openPopup(item) {
     item.classList.add("popup_opened");
 };
 
 function openPopupEditProfileForm() {
-    popupEditProfile.classList.add('popup_opened');
+    openPopup(popupEditProfile);
     profileNameInput.value = profileName.innerText;
     profileSubtitleInput.value = profileSubtitle.innerText;
   }
@@ -82,110 +53,103 @@ function closePopup(item) {
   };
 
 function closePopupEditProfileForm() {
-    popupEditProfile.classList.remove('popup_opened');
+  closePopup(popupEditProfile);
 }
 
 // открытие и закрытие попапа добавления карточки
 
 function openPopupAddItemForm() {
-    popupAddItem.classList.add('popup_opened');
+    openPopup(popupAddItem);
   }
 
 function closePopupAddItemForm() {
-    popupAddItem.classList.remove('popup_opened');
+    closePopup(popupAddItem);
 }
 
 // открытие и закрытие фулксрин картинки
 
 function openFullscreenItem() {
-    popupFullscreenItem.classList.add('popup_opened');
+    openPopup(popupFullscreenItem);
   }
 
 function closeFullscreenItem() {
-    popupFullscreenItem.classList.remove('popup_opened');
+    closePopup(popupFullscreenItem);
 }
 
-
-
 // функция изменения био профиля
-function handleFormSubmit(evt) {
+function handleEditProfileFormSubmit(evt) {
     evt.preventDefault();
     profileName.textContent = profileNameInput.value;
     profileSubtitle.textContent = profileSubtitleInput.value;
-    closePopupEditProfileForm();
+    closePopup(popupEditProfile);
+    // closePopupEditProfileForm();
 }
 
 // геренация фото добавление стрелочной функции 
-    function addNewItem(itemInfo) {
-    const templateItem = elementsTemplate.cloneNode(true);
-    const templateItemImg = templateItem.querySelector(".elements__image");
-    const templateItemTitle = templateItem.querySelector('.elements__title');
-    const templateItemLike = templateItem.querySelector(".elements__like-button");
-    const templateItemRemove = templateItem.querySelector(".elements__remove-button");
-    templateItemLike.addEventListener('click', likeItem);
+function createCard(itemInfo) {
+  const templateItem = elementsTemplate.cloneNode(true);
+  const templateItemImg = templateItem.querySelector(".elements__image");
+  const templateItemTitle = templateItem.querySelector('.elements__title');
+  const templateItemLike = templateItem.querySelector(".elements__like-button");
+  const templateItemRemove = templateItem.querySelector(".elements__remove-button");
+  templateItemLike.addEventListener('click', likeItem);
 
+  
+  templateItemTitle.textContent = itemInfo.name;
+  templateItemImg.alt = itemInfo.name;
+  templateItemImg.src = itemInfo.link;
+  
+  templateItemLike.addEventListener('click', likeItem);
+  templateItemRemove.addEventListener('click', deleteItem);
+
+  function openFullscreenPopup() {
+      popupItemImage.src = templateItemImg.src;
+      popupItemImage.alt = templateItemImg.alt;
+      popupItemTitle.textContent = templateItemTitle.textContent;
+      openFullscreenItem();
+  }
+  
+  templateItemImg.addEventListener('click', openFullscreenPopup);
+  
+  return templateItem;
     
-    templateItemTitle.textContent = itemInfo.name;
-    templateItemImg.src = itemInfo.link;
-    
-    templateItemLike.addEventListener('click', likeItem);
-    templateItemRemove.addEventListener('click', deleteItem);
+  };
 
-    function openFullscreenPopup() {
-        popupItemImage.src = templateItemImg.src;
-        popupItemImage.alt = templateItemImg.alt;
-        popupItemTitle.textContent = templateItemTitle.textContent;
-        openFullscreenItem();
-    }
-    
-    templateItemImg.addEventListener('click', openFullscreenPopup);
-    
-    return templateItem;
-    
-    };
+function deleteItem(event) {
+    event.target.closest('.elements__item').remove();
+}
 
-    function deleteItem(event) {
-        event.target.closest('.elements__item').remove();
-    }
-
-    function submitNewItem(event) {
-        event.preventDefault();
-        renderElements({ 
-            name: addItemNameInput.value,
-            link: addItemLinkInput.value,
-        });
-        event.target.reset();
-        closePopupAddItemForm;
-    }
-
-    function renderElements(itemInfo) {
-        elementsList.prepend(addNewItem(itemInfo));
-    };
-
-    initialCards.forEach((itemInfo) => {
-        renderElements(itemInfo);
+function submitNewItem(event) {
+    event.preventDefault();
+    renderElements({ 
+        name: addItemNameInput.value,
+        link: addItemLinkInput.value,
     });
+    event.target.reset();
+    closePopupAddItemForm();
+}
 
+function renderElements(itemInfo) {
+    elementsList.prepend(createCard(itemInfo));
+};
+
+initialCards.forEach((itemInfo) => {
+    renderElements(itemInfo);
+});
 
 // поставить сердечко
 
 function likeItem(event) {
     event.target.classList.toggle('elements__like-button_active');
 }
-
-
 // обработчики событий и функций
 editProfileInfoButton.addEventListener('click', openPopupEditProfileForm);
 closeProfileInfoButton.addEventListener('click', closePopupEditProfileForm);
 openItemAddButton.addEventListener('click', openPopupAddItemForm);
 closePopupAddItemFormButton.addEventListener('click', closePopupAddItemForm);
-
 closeFullscreenButton.addEventListener('click', closeFullscreenItem);
 
-addItemForm.addEventListener("submit", submitNewItem);
-popupEditProfileForm.addEventListener('submit', handleFormSubmit);
+formAddCard.addEventListener("submit", submitNewItem);
+popupEditProfileForm.addEventListener('submit', handleEditProfileFormSubmit);
 
 openItemAddButton.addEventListener('click', openPopupAddItemForm);
-
-
-
